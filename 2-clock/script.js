a589a22e2e36810b322dc420e7495e8bf25473a9
sBucka <118976633+sBucka@users.noreply.github.com>
2024-06-04 19:32:58 +0200

    Update clock

--- 2-clock/script.js
+++ 2-clock/script.js
@@ -1,36 +1,52 @@
-const secondHand = document.querySelector(".second-hand")
-const minHand = document.querySelector(".min-hand")
-const hourHand = document.querySelector(".hour-hand")
+const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+const tick = document.querySelector(`audio[data-sound="tick"]`);
+const tock = document.querySelector(`audio[data-sound="tock"]`);
+const mutebtn = document.getElementById("mute");
 
-function setDate(){
-    const now = new Date()
-    const seconds = now.getSeconds()
-    const secondsDegrees = ((seconds/60) *360)+90
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`
-
-    const min = now.getMinutes()
-    const minDegrees = ((min/60)*360)+90
-    minHand.style.transform = `rotate(${minDegrees}deg)`
-
-    const hour = now.getHours()
-    const hourDegrees = ((hour/12)*360)+90
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`
+function mute() {
+  if (mutebtn.innerHTML == "mute") {
+    mutebtn.innerHTML = "unmute";
+  } else {
+    mutebtn.innerHTML = "mute";
+  }
 }
 
-const clockFace = document.querySelector(".clock-face")
-for(i = 1; i <13; i++){
-    let div = document.createElement("div")
-    div.classList.add("number")
-    div.classList.add(`n${i}`)
-    div.innerHTML = i
-    clockFace.appendChild(div)
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  if (mutebtn.innerHTML == "mute") {
+    if (seconds % 2 == 0) {
+      tick.currentTime = 0;
+      tick.play();
+    } else {
+      tock.currentTime = 0;
+      tock.play();
+    }
+  }
+
+  const min = now.getMinutes();
+  const minDegrees = (min / 60) * 360 + 90;
+  minHand.style.transform = `rotate(${minDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = (hour / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
-const numbers = document.getElementsByClassName(".number")
-for( i = 0; i < numbers.length(); i++){
-    console.log(numbers[i])
+const clockFace = document.querySelector(".clock-face");
+for (i = 1; i < 13; i++) {
+  let div = document.createElement("div");
+  let div1 = document.createElement("div");
+  div.classList.add("number");
+  div.style.transform = `rotate(${i * 30}deg)`;
+  div1.innerHTML = i;
+  div.appendChild(div1);
+  clockFace.appendChild(div);
 }
 
-
-
-setInterval(setDate, 1000)
+setInterval(setDate, 1000);
