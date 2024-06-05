cf51c7eef74d14a3642b6b6a55b0d8ef266bc662
sBucka <118976633+sBucka@users.noreply.github.com>
2024-06-05 23:20:28 +0200

    Update landing page

--- 1-drum-kit/script.js
+++ 1-drum-kit/script.js
@@ -84,6 +84,7 @@ function removeTransition(e) {
   if (e.propertyName !== "transform") return;
   this.classList.remove("playing");
 }
+
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -92,6 +93,7 @@ function playSound(e) {
   audio.play();
   key.classList.add("playing");
 }
+
 const keys = document.querySelectorAll(".key");
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 window.addEventListener("keydown", playSound);
