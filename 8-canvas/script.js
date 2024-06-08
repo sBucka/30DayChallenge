399528e488575e237e92ff57fd9c20f7aa699df1
sBucka <118976633+sBucka@users.noreply.github.com>
2024-06-08 14:27:02 +0200

    Update Canvas
    Able to use touch

--- 8-canvas/script.js
+++ 8-canvas/script.js
@@ -8,7 +8,7 @@ ctx.strokeStyle = "#BADA55";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
 ctx.lineWidth = 12;
-ctx.globalCompositeOperation = "soft-light"
+ctx.globalCompositeOperation = "soft-light";
 
 let isDrawing = false;
 let lastX = 0;
@@ -16,9 +16,8 @@ let lastY = 0;
 let hue = 0;
 let direction = true;
 
-function draw(e) {
+function drawMouse(e) {
   if (!isDrawing) return; // Stop function
-  console.log(e);
   ctx.strokeStyle = `hsl(${hue},100%,50%)`;
   ctx.beginPath();
   ctx.moveTo(lastX, lastY);
@@ -37,9 +36,10 @@ function draw(e) {
   } else {
     ctx.lineWidth--;
   }
+  //console.log(e);
 }
 
-canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousemove", drawMouse);
 canvas.addEventListener("mousedown", (e) => {
   isDrawing = true;
   lastX = e.offsetX;
@@ -47,3 +47,39 @@ canvas.addEventListener("mousedown", (e) => {
 });
 canvas.addEventListener("mouseup", () => (isDrawing = false));
 canvas.addEventListener("mouseout", () => (isDrawing = false));
+
+function drawTouch(e) {
+  if (!isDrawing) return; // Stop function
+  var touch = e.touches[0];
+
+  ctx.strokeStyle = `hsl(${hue},100%,50%)`;
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(touch.pageX - touch.target.offsetLeft, touch.pageY - touch.target.offsetTop);
+  ctx.stroke();
+  lastX = touch.pageX - touch.target.offsetLeft;
+  lastY = touch.pageY - touch.target.offsetTop;
+
+  hue++;
+  if (hue > 360) hue = 0;
+  if (ctx.lineWidth >= 40 || ctx.lineWidth <= 3) {
+    direction = !direction;
+  }
+  if (direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
+  console.log(lastX, lastY);
+}
+
+canvas.addEventListener("touchmove", drawTouch);
+canvas.addEventListener("touchstart", (e) => {
+  var touch = e.touches[0];
+  isDrawing = true;
+  lastX = touch.pageX - touch.target.offsetLeft;
+  lastY = touch.pageY - touch.target.offsetTop;
+  console.log(lastX, lastY);
+});
+canvas.addEventListener("touchend", () => (isDrawing = false));
+canvas.addEventListener("touchcancel", () => (isDrawing = false));
